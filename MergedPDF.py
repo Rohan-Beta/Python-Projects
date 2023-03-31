bf0b11b524502f209839772d96b8761b427b9cb1
Beta <125117740+Rohan-Beta@users.noreply.github.com>
2023-03-31 12:25:29 +0530

    Create MergedPDF.py

---
+++ MergedPDF.py
@@ -0,0 +1,16 @@
+# merge pdf
+
+import PyPDF2 # this library is used to merge pdf
+
+pdfFiles = ["sample.pdf" , "file-sample.pdf"] # store two pdf files
+
+merger = PyPDF2.PdfMerger() # meged two pdf
+
+for file in pdfFiles : # access all the files in pdf
+    pdfFiles2 = open(file , "rb") # open pdf
+    
+    pdfReader = PyPDF2.PdfReader(pdfFiles2) # read the pdf files
+    merger.append(pdfReader) # append and merged
+    
+pdfFiles2.close() # close the pdf
+merger.write("merged.pdf") #create new file to store merged pdf
