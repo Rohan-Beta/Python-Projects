649d9b2d3d50f84755e55bee22ea8093cba6e4ca
Beta <125117740+Rohan-Beta@users.noreply.github.com>
2023-11-18 13:52:05 +0530

    Update PhNoLocation.py

--- PhNoLocation.py
+++ PhNoLocation.py
@@ -3,7 +3,7 @@
 import phonenumbers
 from phonenumbers import geocoder
 
-ph1 = phonenumbers.parse("+918017") // put the number
+ph1 = phonenumbers.parse("+918017") # put the number
 
 print("Phone Number Location:")
-print(geocoder.description_for_number(ph1 , "en")) // print the phone number location
+print(geocoder.description_for_number(ph1 , "en")) # print the phone number location
